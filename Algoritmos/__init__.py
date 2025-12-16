c9550a531a5fe66ecea291150ad0ae77e8f5b324
ama9-cin-ufpe <ama9@cin.ufpe.br>
2025-12-16 13:42:16 -0300

    Update __init__.py
    Atualização do arquivo de algoritmos

--- Algoritmos/__init__.py
+++ Algoritmos/__init__.py
@@ -1,4 +1,6 @@
+import pygame
 from random import shuffle
+import Constantes as k
 
 class AbstractMinefield:
 
@@ -12,7 +14,7 @@ class AbstractMinefield:
         self.bombs = int(self.tiles * density)
         self.free_tiles = self.tiles - self.bombs
         self.discovered_tiles = 0
-        self.win = self.trip = False
+        self.win = False
         self.first_dig = True
 
     def fill_minefield(self):
@@ -63,6 +65,8 @@ class AbstractMinefield:
                     self.bombs -= 1
                     self.free_tiles += 1
                     self.discovered_tiles += 1
+                    post_event = pygame.event.Event(k.MINESWEEPER_HIT, {'coordinates': (i_tile, j_tile)})
+                    pygame.event.post(post_event)
                     self.count_all()
 
                 # A PRIMEIRA ESCAVAÇÃO ACERTOU UM LADRILHO LIVRE
@@ -70,6 +74,13 @@ class AbstractMinefield:
                     self.count_all()
                     self.minefield_interface[i_tile][j_tile] = 1
                     self.discovered_tiles += 1
+                    post_event = pygame.event.Event(k.MINESWEEPER_HIT, {'coordinates': (i_tile, j_tile)})
+                    pygame.event.post(post_event)
+
+                # A ESCAVAÇÃO NÃO FOI REALIZADA PELA CASCATA DE ESCAVAÇÃO
+                if primary_dig:
+                    post_event = pygame.event.Event(k.MINESWEEPER_PRIMARY_HIT, {'coordinates': (i_tile, j_tile)})
+                    pygame.event.post(post_event)
 
                 # OS LADRILHOS AO REDOR SERÃO ESCAVADOS CASO NÃO HAJAM BOMBAS POR PERTO
                 for i, j in ((-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)):
@@ -78,12 +89,19 @@ class AbstractMinefield:
 
             # ACERTOU UMA MINA SEM SER A PRIMEIRA ESCAVAÇÃO
             elif self.minefield[i_tile][j_tile]:
-                self.trip = True
+                post_event = pygame.event.Event(k.MINESWEEPER_MISS)
+                pygame.event.post(post_event)
 
             # REVELOU UM LADRILHO SEM SER A PRIMEIRA ESCAVAÇÃO
-            else:
-                self.minefield_interface[i_tile][j_tile] = 1
-                self.discovered_tiles += 1
+            elif not self.minefield_interface[i_tile][j_tile]:
+                if primary_dig:
+                    post_event = pygame.event.Event(k.MINESWEEPER_PRIMARY_HIT, {'coordinates': (i_tile, j_tile)})
+                    pygame.event.post(post_event)
+                if not (self.minefield_interface[i_tile][j_tile] or self.minefield[i_tile][j_tile]):
+                    self.minefield_interface[i_tile][j_tile] = 1
+                    self.discovered_tiles += 1
+                    post_event = pygame.event.Event(k.MINESWEEPER_HIT, {'coordinates': (i_tile, j_tile)})
+                    pygame.event.post(post_event)
                 for i, j in ((-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)):
                     if self.height > i_tile + i >= 0 and self.width > j_tile + j >= 0 and self.minefield_interface[i_tile + i][j_tile + j] != 1 and not self.minefield_mask[i_tile][j_tile]:
                         self.dig((i_tile + i, j_tile + j), False)
@@ -92,12 +110,18 @@ class AbstractMinefield:
     def flag(self, tile):
         i_tile, j_tile = tile
         view = self.minefield_interface[i_tile][j_tile]
-
         if view == 0:
             self.minefield_interface[i_tile][j_tile] = 2
+            post_event = pygame.event.Event(k.MINESWEEPER_FLAG, {'coordinates': (i_tile, j_tile)})
+            pygame.event.post(post_event)
         elif view == 2:
             self.minefield_interface[i_tile][j_tile] = 0
+            post_event = pygame.event.Event(k.MINESWEEPER_UNFLAG, {'coordinates': (i_tile, j_tile)})
+            pygame.event.post(post_event)
 
-    # REINICIA O INDICADOR DE ERRO
-    def reset_trip(self):
-        self.trip = False
+    # VERIFICA SE O CAMPO MINADO FOI COMPLETAMENTE LIMPO
+    def win_check(self):
+        if not self.win and self.discovered_tiles == self.free_tiles:
+            self.win = True
+            post_event = pygame.event.Event(k.MINESWEEPER_WIN)
+            pygame.event.post(post_event)
